6cb21c8fa40edb99fc7e5f5851b07ccbd0adf895
Farid0558475484 <109238744+Farid0558475484@users.noreply.github.com>
2022-08-03 16:02:17 +0400

    3

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Array.verilir.arrayin.ən.kicik.elementini.tapan.algoritm.yazmag
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int[] arr = { 144, 23525, 456, 242, 5576, 2424, 75, 635, 4325, 735 };
+            int min = arr[0];
+
+
+
+            for (int i = 0; i <arr.Length; i++)
+            {
+                if (arr[i]<min)
+
+                {
+                    min = arr[i];
+                }
+
+            }
+            Console.WriteLine(min);
+        }
+
+
+    }
+}
+
+
+
+
+
